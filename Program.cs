7ac3396ca0ea4fab9895b6a3d91816a8d2979be2
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-29 20:47:52 -0300

    pasta de cidades

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Pressione qualquer tecla para ver as cidades...");
+Console.ReadKey();
+Console.WriteLine("São Vicente");
+Console.ReadKey();
+Console.WriteLine("Itanhaém");
+Console.ReadKey();
+Console.WriteLine("Mongaguá");
+Console.ReadKey();
+Console.WriteLine("Santos");
+Console.ReadKey();
+Console.WriteLine("Praia Grande");
+Console.ReadKey();
+
+
+
+
+
